bc3f934b0c86576b946b1dfd285064361eaff436
joaonorr <joaonoroes.jn@gmail.com>
2021-10-01 23:30:06 -0300

    C++ resolution 1.1

--- Estressados_norr/stressed.cpp
+++ Estressados_norr/stressed.cpp
@@ -104,14 +104,112 @@ int procurar_melhor_se( std::vector<int> vet ){
     return min;
 }
 
+float calcular_stress_medio(std::vector<int> vet ){
+    int soma = 0;
+    
+    for( int i =0; i < (int) vet.size(); i++ ){
+        if( vet[i] < 0 ){
+            vet[i] *= -1;
+        }
+        soma += vet[i];
+    }
+
+    return (soma/(int) vet.size());
+}
+
+std::string mais_homens_ou_mulheres(std::vector<int> vet){
+
+    int mulheres = 0;
+
+    for( int i = 0; i < vet.size(); i++){
+        if( vet[i] < 0 ){
+            mulheres++;
+        }
+    }
+
+    if( mulheres == vet.size()/2){
+        return "empate";
+    }
+    else{
+        if ( mulheres > vet.size()/2){
+            return "mulheres";
+        }
+        else {
+            return "homens";
+        }
+    }
+}
+
+std::string qual_metade_eh_mais_estressad(std::vector<int> vet){
+
+    int all = 0;
+    int primeiro = 0;
+    int segundo = 0;
+
+    for( int i =0; i < (int) vet.size(); i++ ){
+        if( vet[i] < 0 ){
+            vet[i] *= -1;
+        }
+        
+        all += vet[i];
+        if( i < vet.size()/2 ){
+            primeiro += vet[i];
+        }
+        else{
+            segundo += vet[i];
+        }
+    }
+
+    if( primeiro == segundo ){
+        return "empate";
+    }
+    else{
+        if( primeiro < segundo ){
+            return "segunda";
+        }
+        else {
+            return "primeira";
+        }
+    }
+
+}
+
+std::string homens_sao_mais_estressados_que_mulheres(std::vector<int> vet){
+    int mulher = 0;
+    int homem = 0;
+    int cont_homem = 0;
+
+    for( int i =0; i < (int) vet.size(); i++ ){
+        if(vet[i] < 0){
+            homem += vet[i];
+            cont_homem++;
+        }
+        else{
+            mulher += vet[i] * -1;
+        }
+    }
+
+    if( (homem / cont_homem) == (mulher / ( (int) vet.size() - cont_homem ))){
+        return "empate";
+    }
+
+    if( homem / cont_homem < mulher / ( (int) vet.size() - cont_homem )){
+        return "não";
+    }
+    else{
+        return "sim";
+    }
+    
+}
+
 int main(){
 
-    int x {};
+    int x = -50;
     int pos_inicial = 3;
     int p = 3;
     
     // Módulo de Busca
-    std::cin >> x ;
+    //std::cin >> x ;
     std::cout << existe({-1, -50, -99, 80, 70, 90, -4}, x ) << "\n" ;
     std::cout << contar({-1, -50, -99, 80, 70, 90, -4}, x ) << "\n" ;
     std::cout << procurar_valor({-1, -50, -99, 80, 70, 90, -4}, x ) << "\n" ;
@@ -124,6 +222,11 @@ int main(){
     std::cout << procurar_menor_poss_apos( {-1, -50, -99, 80, 70, 90, -4},p ) << "\n" ;
     std::cout << procurar_melhor_se( {-1, -50, -99, 80, 70, 90, -4} ) << "\n" ;
 
+    // Módulo de contagem
+    std::cout << calcular_stress_medio( {-1, -50, -99, 80, 70, 90, -4} ) << "\n" ;
+    std::cout << mais_homens_ou_mulheres( {-1, -50, -99, 80, 70, 90, -4} ) << "\n" ;
+    std::cout << qual_metade_eh_mais_estressad( {5, 3, -1, -50, -1, -99} ) << "\n" ;
+    std::cout << homens_sao_mais_estressados_que_mulheres( {50, 98, 2, -50, -1, -99} ) << "\n" ;
 
 
 
